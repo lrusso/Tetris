168405644d70fbbf3906a31420b9a22808e1d453
Leonardo Russo <info@lrusso.com>
2021-12-01 19:07:51 -0300

    Add files via upload

--- TetrisGame.js
+++ TetrisGame.js
@@ -13,7 +13,7 @@ function isWebGLAvailable(){if(window.WebGLRenderingContext){for(var e=document.
 // isMobileDevice.js
 function isMobileDevice(){return!!(navigator.userAgent.match(/Android/i)||navigator.userAgent.match(/webOS/i)||navigator.userAgent.match(/iPhone/i)||navigator.userAgent.match(/iPad/i)||navigator.userAgent.match(/iPod/i)||navigator.userAgent.match(/BlackBerry/i)||navigator.userAgent.match(/Windows Phone/i))}
 
-var STRING_GAMEOVER = "Game Over.";
+var STRING_GAMEOVER = "GAME OVER";
 
 var GAME_SOUND_ENABLED = false;
 
