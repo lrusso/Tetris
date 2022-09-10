4545dd7d547153a00471ccefce825c8dc81cce91
Leonardo Russo <info@lrusso.com>
2022-09-09 21:18:00 -0300

    Add files via upload

--- TetrisGame.js
+++ TetrisGame.js
@@ -305,7 +305,7 @@ Tetris.Menu.prototype = {
 				{
 				var date = new Date();
 				date.setTime(date.getTime() + (days*24*60*60*1000));
-				expires = "; expires=" + date.toUTCString();
+				expires = "; expires=" + date.toUTCString() + "; SameSite=Lax";
 				}
 			document.cookie = name + "=" + (value || "")  + expires + "; Secure; path=/";
 			}
